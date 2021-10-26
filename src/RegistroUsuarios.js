010db772fdd5e7c00a0ed16d987fe29ab5902578
Camilo Ortiz <camiloorvi@gmail.com>
2021-10-26 02:46:44 -0500

    Se agrega boton salir y panel en registro usuario

--- src/RegistroUsuarios.js
+++ src/RegistroUsuarios.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import LogoutButton from './logout-button.js';
+import { Link } from 'react-router-dom';
+
 class Roles extends React.Component {
     sidebarToggle() {
         var sidebar = document.getElementById('sidebar');
@@ -39,11 +41,18 @@ render() {
             <aside id="sidebar" class="bg-side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block">
                 <ul class="list-reset flex flex-col">
                     <li class=" w-full h-full py-3 px-2 border-b border-light-border bg-white">
-                        <a href="#"
+                        <a href="/dashboard"
                            class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fas fa-tachometer-alt float-left mx-2"></i>
-                           Panel de Control
+                           Panel Principal
                             <span><i class="fas fa-angle-right float-right"></i></span>
+                        </a>
+                    </li>
+                    <li class=" w-full h-full py-3 px-2 border-b border-light-border bg-white">
+                        <a href="#"
+                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
+                            <i class="fas fa-sign-out-alt float-left mx-2"></i>
+                            <LogoutButton />
                         </a>
                     </li>
                     
