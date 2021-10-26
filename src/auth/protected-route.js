bccdb933ecb163f954eb8ce1a79db5b4226fdfeb
Camilo Ortiz <camiloorvi@gmail.com>
2021-10-26 02:13:59 -0500

    Se modifica protected route

--- src/auth/protected-route.js
+++ src/auth/protected-route.js
@@ -7,7 +7,9 @@ import { withAuthenticationRequired } from '@auth0/auth0-react';
 
 const ProtectedRoute = ({ component, ...args }) => (
   <Route
-    component={withAuthenticationRequired}
+    component={withAuthenticationRequired(component, {
+        onRedirecting: () => <Auth />,
+      })}
     {...args}
   />
 );
